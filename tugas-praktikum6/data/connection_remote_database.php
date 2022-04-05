05a3180e500f371c1c3dde47585ce82ee982fd94
Harvian Arga Adi Putra <99303471+Harvian25@users.noreply.github.com>
2022-04-05 13:36:27 +0700

    Add files via upload

---
+++ tugas-praktikum6/data/connection_remote_database.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$database = "pbw-praktik";
+$username = "root";
+$password = "12345678";
+
+$connection = mysqli_connect($servername, $username, $password, $database);
